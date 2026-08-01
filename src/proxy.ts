475917d27e4206f2e8f1286ec019242b700fae23
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-01 22:50:30 +0530

    feat(middleware): implement route protection and authorization proxy
    Add a proxy middleware to handle route-based access control. The implementation includes:
    - Public route definitions for home and authentication endpoints
    - Role-based redirection for `/admin` and `/partner` routes
    - Authentication checks for all `/api` routes to prevent unauthorized access
    - Exclusion rules for static assets and internal Next.js paths

---
+++ src/proxy.ts
@@ -0,0 +1,61 @@
+import { useSession } from "next-auth/react"
+import { NextURL } from "next/dist/server/web/next-url"
+import { NextRequest, NextResponse } from "next/server"
+import { auth } from "./auth"
+
+const PUBLIC_ROUTES = ["/"]
+
+export const proxy=async(req:NextRequest)=>{
+const {pathname}=await req.nextUrl
+const session=await auth()
+const role=session?.user?.role
+
+
+if(   
+pathname.startsWith("/next")|| pathname.startsWith("/favicon.icon")||
+  /\.(png|jpg|jpeg|gif|svg|webp|ico)$/i.test(pathname)
+
+){
+    return NextResponse.next()
+
+}
+
+
+if(PUBLIC_ROUTES.includes('/api/auth')){
+    return NextResponse.next()
+}
+
+if(PUBLIC_ROUTES.includes(pathname)){
+    return NextResponse.next()
+}
+
+if(pathname.startsWith("/admin")){
+    if(role!="admin"){
+        return NextResponse.redirect(new URL("/", req.url))
+    }
+}
+
+if(pathname.startsWith("/partner")){
+    if(role!="partner"){
+        return NextResponse.redirect(new URL("/",req.url))
+    }
+}
+if(pathname.startsWith("/api")){
+    if(!session?.user){
+        return NextResponse.json({
+            message:"unauthrize",
+            status:401
+        })
+    }
+}
+
+return NextResponse.next()
+
+}
+
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\..*).*)",
+  ],
+};
