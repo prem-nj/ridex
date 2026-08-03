4e5328ffba31b8a3a75655b1d05bebfc9ff01455
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-03 08:54:09 +0530

    build(deps): add zego-uikit-prebuilt and update home page formatting
    - include @zegocloud/zego-uikit-prebuilt in project dependencies
    - add whitespace to src/app/page.tsx for improved code structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import PublicHome from "@/components/PublicHome";
 import Image from "next/image";
 
 export default function Home() {
+
   return (
 <div className="w-full min-h screen bg-white">
 <Navbar/>
