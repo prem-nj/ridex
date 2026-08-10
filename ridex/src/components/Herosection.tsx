1ba3434e62730c55b6e85957de0775dce8831c1e
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-10 11:49:48 +0530

    Add RootState type and onAuthType

--- ridex/src/components/Herosection.tsx
+++ ridex/src/components/Herosection.tsx
@@ -5,7 +5,7 @@ import { motion, scale } from "motion/react"
 import { Bike, Bus, Car, Truck } from 'lucide-react';
 import { useRouter } from 'next/navigation'
 import { useSelector } from 'react-redux'
-
+import type { RootState } from '@/redux/store'
 type onAuthType={
   onAuthRequired:()=>void
 }
