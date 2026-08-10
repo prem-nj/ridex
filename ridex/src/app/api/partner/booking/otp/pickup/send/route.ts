d77daf534dcec0201bd64dabf6d5a417afc4a7a5
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-10 19:41:25 +0530

    fix pickup OTP route request import

--- ridex/src/app/api/partner/booking/otp/pickup/send/route.ts
+++ ridex/src/app/api/partner/booking/otp/pickup/send/route.ts
@@ -1,3 +1,9 @@
+
+import connectDb from "@/lib/db";
+import { sendMail } from "@/lib/sendMail";
+import Booking from "@/models/booking.model";
+import { NextRequest, NextResponse } from "next/server";
+
 export async function POST(req: NextRequest) {
     try {
         console.log("1. Starting pickup OTP");
