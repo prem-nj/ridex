d0eba898d3bb1f15418e19d8541297ef3d1a1ec3
Prem Navnath Jadhav <premjadhav21314@gmail.com>
2026-08-10 21:37:19 +0530

    Update email password environment variable key

--- ridex/src/lib/sendMail.ts
+++ ridex/src/lib/sendMail.ts
@@ -4,7 +4,7 @@ const transporter=nodemailer.createTransport({
     service:"gmail",
     auth:{
         user:process.env.EMAIL,
-        pass:process.env.PASSWORD
+        pass:process.env.PASS
     }
 })
 
